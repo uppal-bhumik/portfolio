0d220cb6bd8c8b909ba4e98dd1b0488bf026e567
Bhumik Uppal <144423161+uppal-bhumik@users.noreply.github.com>
2025-10-29 15:27:21 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -87,7 +87,7 @@ export default function Hero() {
                   viewBox="0 0 24 24" 
                   stroke="currentColor"
                 >
-                  <path d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                  <path d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
                 Resume
               </a>
@@ -109,7 +109,7 @@ export default function Hero() {
             viewBox="0 0 24 24" 
             stroke="currentColor"
           >
-            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+            <path d="M19 14l-7 7m0 0l-7-7m7 7V3" />
           </svg>
         </div>
       </div>
